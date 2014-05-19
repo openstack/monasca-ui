58c97445c8d55d48e03498c758f7b7c6dee245aa
Rob Raymond <rob.raymond@hp.com>
2014-05-19 10:31:46 -0600

    Set DEFAULT_PANEL to monitoring panel

--- enabled/_50_admin_add_monitoring_panel.py
+++ enabled/_50_admin_add_monitoring_panel.py
@@ -5,10 +5,15 @@ PANEL_DASHBOARD = 'overcloud'
 # The name of the panel group the PANEL is associated with.
 #PANEL_GROUP = 'admin'
 
+DEFAULT_PANEL = 'monitoring'
+
 # Python panel class of the PANEL to be added.
 ADD_PANEL = \
     'monitoring.panel.Monitoring'
 
 # A list of applications to be added to INSTALLED_APPS.
 ADD_INSTALLED_APPS = ['monitoring']
+
+# A list of angular modules to be added as dependencies to horizon app.
+#ADD_ANGULAR_MODULE = ['monitoringApp']
 
