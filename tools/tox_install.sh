bdb475dda1f880d2951382f9b07b97fa18e224bc
Thomas Bechtold <tbechtold@suse.com>
2017-04-10 12:08:14 +0200

    Sync tools/tox_install.sh
    Sync tools/tox_install.sh with python-openstackclient. This brings in
    the following improvements:
    
    * Do not leave temporary directory around, instead delete temporary
      directory directly after usage (change
      I939eae82dba3287fd4e4086128ebf4609a0e0770).
    * Do not set ZUUL_BRANCH explicitely and remove unused if condition
      (change I0077c986a17d6bb92791474e03d1e77776e9382f).
    
    Change-Id: Ie56634fcb4dcf670510771efffccb4214c87359c
    (cherry picked from commit e27a1c99709f5656927943c4ea114a542add9147)

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -1,30 +1,57 @@
 #!/usr/bin/env bash
 
-# Client constraint file contains this client version pin that is in conflict
-# with installing the client from source. We should remove the version pin in
+# Constraint file contains this package version pin that is in conflict
+# with installing the package from source. We should replace the version pin in
 # the constraints file before applying it for from-source installation.
 
-CONSTRAINTS_FILE="$1"
-shift 1
+ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
+BRANCH_NAME=stable/ocata
+PACKAGE_NAME=monasca-ui
+requirements_installed=$(echo "import openstack_requirements" | python 2>/dev/null ; echo $?)
 
 set -e
 
-# NOTE(tonyb): Place this in the tox enviroment's log dir so it will get
-# published to logs.openstack.org for easy debugging.
-localfile="$VIRTUAL_ENV/log/upper-constraints.txt"
+git config --global url.https://git.openstack.org/.insteadOf git://git.openstack.org/
 
-if [[ "$CONSTRAINTS_FILE" != http* ]]; then
-    CONSTRAINTS_FILE="file://$CONSTRAINTS_FILE"
+CONSTRAINTS_FILE=$1
+shift
+
+install_cmd="pip install"
+mydir=$(mktemp -dt "$PACKAGE_NAME-tox_install-XXXXXXX")
+trap "rm -rf $mydir" EXIT
+localfile=$mydir/upper-constraints.txt
+if [[ $CONSTRAINTS_FILE != http* ]]; then
+    CONSTRAINTS_FILE=file://$CONSTRAINTS_FILE
 fi
-# NOTE(tonyb): need to add curl to bindep.txt if the project supports bindep
-curl "$CONSTRAINTS_FILE" --insecure --progress-bar --output "$localfile"
+curl $CONSTRAINTS_FILE -k -o $localfile
+install_cmd="$install_cmd -c$localfile"
 
-pip install -c"$localfile" openstack-requirements
+if [ $requirements_installed -eq 0 ]; then
+    echo "ALREADY INSTALLED" > /tmp/tox_install.txt
+    echo "Requirements already installed; using existing package"
+elif [ -x "$ZUUL_CLONER" ]; then
+    echo "ZUUL CLONER" > /tmp/tox_install.txt
+    pushd $mydir
+    $ZUUL_CLONER --cache-dir \
+        /opt/git \
+        --branch $BRANCH_NAME \
+        git://git.openstack.org \
+        openstack/requirements
+    cd openstack/requirements
+    $install_cmd -e .
+    popd
+else
+    echo "PIP HARDCODE" > /tmp/tox_install.txt
+    if [ -z "$REQUIREMENTS_PIP_LOCATION" ]; then
+        REQUIREMENTS_PIP_LOCATION="git+https://git.openstack.org/openstack/requirements@$BRANCH_NAME#egg=requirements"
+    fi
+    $install_cmd -U -e ${REQUIREMENTS_PIP_LOCATION}
+fi
 
 # This is the main purpose of the script: Allow local installation of
 # the current repo. It is listed in constraints file and thus any
 # install will be constrained and we need to unconstrain it.
-edit-constraints "$localfile" -- "$CLIENT_NAME"
+edit-constraints $localfile -- $PACKAGE_NAME "-e file://$PWD#egg=$PACKAGE_NAME"
 
-pip install -c"$localfile" -U "$@"
+$install_cmd -U $*
 exit $?
